a2a9008d8bab5ac0fe0c26481b5cec293117cff1
Divyesh Vekariya <170543109018@darshan.ac.in>
2021-04-07 09:03:37 +0530

    user stored in core data and add user functionality.

--- Cards/ContentView.swift
+++ Cards/ContentView.swift
@@ -9,90 +9,86 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    @Environment(\.managedObjectContext) var context
+    @FetchRequest(entity: UserInfo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \UserInfo.name, ascending: true)]) var users: FetchedResults<UserInfo>
+    @State private var isShowAddUser = false
     
-    @State var users:[User] = []
+    @State var usersCard:[User] = []
     
     var body: some View {
-        List {
-            ForEach(users) { user in
-                VStack(alignment: .leading) {
-                    Text(user.name)
-                        .font(.headline)
-                        .fontWeight(.bold)
-                    Text(user.email)
-                        .font(.headline)
-                        .fontWeight(.medium)
-                        .foregroundColor(Color.gray)
-                    Text(user.city)
-                        .font(.subheadline)
-                        .fontWeight(.regular)
-                        .foregroundColor(Color.gray)
+        NavigationView {
+            List {
+                ForEach(usersCard) { user in
+                    VStack(alignment: .leading) {
+                        Text(user.name)
+                            .font(.headline)
+                            .fontWeight(.bold)
+                        Text(user.email)
+                            .font(.headline)
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.gray)
+                        Text(user.city)
+                            .font(.subheadline)
+                            .fontWeight(.regular)
+                            .foregroundColor(Color.gray)
+                    }
                 }
+                .onDelete(perform: deleteUser)
             }
-            .onDelete(perform: deleteItems)
-        }
-        .onAppear {
-            Api().getUsers { (retrivedUsers ) in
-                users.append(contentsOf: retrivedUsers)
+            //.listStyle(GroupedListStyle())
+            .navigationBarTitle("User")
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button("Add") {
+                                    self.isShowAddUser.toggle()
+                                })
+            .sheet(isPresented: $isShowAddUser) {
+                AddView(isShow: $isShowAddUser).environment(\.managedObjectContext,
+                                      self.context)
             }
         }
         
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
+        .onAppear {
+            print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+            Api().getUsers { (retrivedUsers ) in
+                usersCard.append(contentsOf: retrivedUsers)
+                saveUserToCoreData()
+                
+                
             }
         }
-
+        
     }
-    
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
+    private func saveUserToCoreData() {
+        for user in usersCard {
+            let newUser = UserInfo(context: self.context)
+            newUser.name = user.name
+            newUser.email = user.email
+            newUser.city = user.city
             do {
-                try viewContext.save()
+                try self.context.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                print("user not saved \(error.localizedDescription)")
             }
+            
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
+    private func deleteUser(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            for index in offsets {
+                let user = users[index]
+                context.delete(user)
+                do {
+                    try context.save()
+                } catch {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    let nsError = error as NSError
+                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                }
             }
         }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
